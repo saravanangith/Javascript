493524289fb27c5b3be2c0263ff823f369006d8a
saravanangith <saravananadithiya99@gmail.com>
2022-08-13 12:19:10 +0530

    functions

---
+++ 10_08_2022/function.js
@@ -0,0 +1,51 @@
+// functions -> Non primitive (referrence or value datatype)
+
+// with parameter with return type
+// function arithmetic(num2, num1) {
+//   console.log(num2, num1);
+//   let sum = num1 + num2;
+//   let sub = num1 - num2;
+//   let mul = num1 * num2;
+//   let quo = Math.floor(num1 / num2);
+//   let rem = num1 % num2;
+
+//   console.log(sum, sub, mul, quo, rem);
+//   return 10;
+// }
+
+// console.log("returned value", arithmetic(11, 2));
+
+// types -> 4
+// ################################################
+// 1.withparameter without returntype
+
+// function welcome(name) {
+//   console.log("Welcome " + name);
+// }
+
+// welcome("vasanth");
+// welcome("saravanan");
+// welcome("pavithra");
+// welcome("sriram");
+// welcome("soundappan");
+// ####################################
+// 2.without parameter with returntype
+// function getWelcomeMsg() {
+//   return "Welcome";
+// }
+
+// console.log(getWelcomeMsg());
+// ############################################
+// 3.without parameter without return type
+
+// function getWelcomeMsg() {
+//   console.log("Welcome");
+// }
+
+// getWelcomeMsg();
+
+// ######################################
+
+// function add() {}
+
+// console.log(typeof add);
