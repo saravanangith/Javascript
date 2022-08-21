cdaab49d0c690330964362bea437966b0c40d7af
saravanangith <saravananadithiya99@gmail.com>
2022-08-21 21:13:55 +0530

    array

---
+++ 21_08_2022_mrngcls/array.js
@@ -0,0 +1,21 @@
+// datastucture-array,linkedlist,stack,queue-edhelam linear data structures
+// array-searching and sorting ku best hu  [add and  deleting ku not best-disadvantage]
+// add and delete  overcome panurathukuthan linked list vanthuthu but edhula search panurathu romba slow
+// ha erukum and reverse varuthum mudiyathu so evanga double linked list use panaga etha use pana \
+// reverse varalam ana search panurathu sloe ha than erukum
+
+// stack first in last out-push aprm last in first out -pop
+// queue-first in first out last in last out eg-theater la ticket vangurathu
+// queue la last ha value insert pana push method last value ha edutha pop
+// queue la first value ha insert pana unshift first value ha edutha shift
+// what is javascript-single threaded synchronus language- erundu peru hotel ku vranga
+// oruthan idly order panuran enoruthan pizza order panuran order  edukuravan tha enga thread
+// cooking section po erandu order um kudthutu edhu frst  process agi mudikutho atha kuduthurvan enga
+// idly process agi mudkum.
+// but javascript la array na stack,queue,linkedlist eppidi neraya data struture ha join panni array form panirupanga
+// array type vanthu object hu
+// array->Contiguous memory allocation-appidina pakathu pathula than value store agum
+// algorithm animation-https://visualgo.net/en
+let nums = [10, 20, 30, "vasanth", true, null, undefined];
+console.log(typeof nums);
+console.log(nums[0]);
